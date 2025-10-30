6bfcb142308f297246c05fb55de608f0f1198426
Yahor <zloyman212906@gmail.com>
2025-10-30 11:38:11 +0300

    Переделали GenerateData

--- src/GenerateData.java
+++ src/GenerateData.java
@@ -1,18 +1,23 @@
-import java.io.DataOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.awt.image.DataBuffer;
+import java.nio.*;
+import java.nio.channels.*;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+import java.io.*;
 
 public class GenerateData {
-    public static void main(String[] args) {
-        try (DataOutputStream out = new DataOutputStream(new FileOutputStream("abs.bin"))) {
-            for (double x = -10; x <= 10; x += 0.5) {
-                double y = Math.abs(x);
-                out.writeDouble(x);
-                out.writeDouble(y);
-            }
-            System.out.println("Файл abs.bin успешно создан!");
-        } catch (IOException e) {
-            e.printStackTrace();
+    public static void main(String[] args) throws IOException {
+        ByteBuffer byteBuffer = ByteBuffer.allocate(41 * 2 * 8);
+        DoubleBuffer doubleBuffer = byteBuffer.asDoubleBuffer();
+
+        for (double x = -10.0; x <= 10; x += 0.5) {
+            doubleBuffer.put(x);
+            doubleBuffer.put(Math.abs(x));
         }
+
+        doubleBuffer.flip();
+        FileChannel fileChannel = FileChannel.open(Path.of("DataByBuffer.bin"), StandardOpenOption.CREATE, StandardOpenOption.WRITE);
+        fileChannel.write(byteBuffer);
     }
+
 }
